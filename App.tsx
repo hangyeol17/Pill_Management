756542338e4077b176e182068cc3102512f49768
koeyhi <104114941+koeyhi@users.noreply.github.com>
2023-06-05 16:44:32 +0900

    login -> stacktab

--- App.tsx
+++ App.tsx
@@ -1,17 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { ScrollView, Dimensions, SafeAreaView, StyleSheet, ActivityIndicator, Button, View, PermissionsAndroid } from 'react-native'
 import MainPage from './src/screens/Main'
-import Calendar from './src/screens/Calendar'
-import Setting from './src/screens/Setting'
 import Loading from './src/screens/Loading'
 import Login from './src/screens/Login'
 import Register from './src/screens/Register'
-import Join from './src/screens/Join'
-import InputInfo from './src/screens/InputInfo'
-import AddPill from './src/screens/AddPill'
 import StackTab from './src/screens/StackTab'
 import 'react-native-gesture-handler'
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer, useNavigation } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 //푸시 알람
@@ -23,19 +18,15 @@ const numOfCom = 4
 export default function App() {
   const [isLoading, setIsLoading] = useState(true) //true면 로딩창, false면 메인창이 뜬다.
   const children = [<Loading />, <MainPage />]
-  //return (isLoading ? (children[0]) : (children[1])) //로딩이 완료되면 메인페이지로
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
+      <Stack.Navigator initialRouteName='Login' screenOptions={{ headerShown: false }}>
         <Stack.Screen name='Login' component={Login} />
         <Stack.Screen name='Register' component={Register} />
+        <Stack.Screen name='StackTab' component={StackTab} />
       </Stack.Navigator>
     </NavigationContainer>
-    //<AddPill />
-    //<Login />
-    //<Join />
-    //<InputInfo />
   )
 }
 
